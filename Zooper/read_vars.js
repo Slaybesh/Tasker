4a1b74af3e30eb986f447a35211bbef9861ef43a
Huy Hoang <huyhoang0496@gmail.com>
2019-02-25 15:21:15 +0100

    zooper

--- Zooper/read_vars.js
+++ Zooper/read_vars.js
@@ -1,13 +1,18 @@
-let json = JSON.parse(json_str)
-
 function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-for (let key in json) {
-    // let val = '`${json[key]}`'
-    // eval(`var ${key} = ${val}`)
-    setGlobal(capitalizeFirstLetter(key), json[key])
-}
+jQuery.ajaxSetup({async:false});
+
+// let json_str;
+$.get('https://dl.dropboxusercontent.com/1/view/ll1opj7lz3vljn4/boxifier/tasker/Zooper/zooper_variables.txt', (data) => {
+    // flash(data)
+    // json_str = data;
+    let json = JSON.parse(data);
+    
+    for (let key in json) {
+        setGlobal(capitalizeFirstLetter(key), json[key])
+    }
+});
 
 exit()
--- Zooper/zooper.py
+++ Zooper/zooper.py
@@ -12,8 +12,8 @@ class clock_widget:
         self.circumference = int(round(2 * math.pi * self.radius_ring))
         self.y_offset = self.radius_ring + self.y_offset_circle
 
-        self.angle_sunrise = f'({sr_in_min} / {mins_in_day}) * 360 + 180'
-        self.angle_sunset = f'({ss_in_min} / {mins_in_day}) * 360 + 180'
+        self.angle_sunrise = f'(({sr_in_min} / {mins_in_day}) * 360 + 180)'
+        self.angle_sunset = f'(({ss_in_min} / {mins_in_day}) * 360 + 180)'
 
 
         self.text_sun = ''
@@ -23,48 +23,42 @@ class clock_widget:
         self.bar_thickness = 2
         self.bar_length = 30
 
+        print(self.circumference, '\n')
+
         self.print_all()
 
 
     def print_all(self):
 
-        time = self.time()
-        time_circle = self.time_circle()
-        time_indicator = self.time_indicator()
-
-        sr_bar = self.sun_bars(self.angle_sunrise)
-        ss_bar = self.sun_bars(self.angle_sunset)
-
-        sr_time = self.sun_times('rise')
-        ss_time = self.sun_times('set')
-
-        sr_hiding_box = self.sun_hiding_boxes('rise')
-        ss_hiding_box = self.sun_hiding_boxes('set')
-
-        sun = self.sun()
-
         var_dict = {
-            'time': time,
-            'time_circle': time_circle,
-            'time_indicator': time_indicator,
+            'time': self.time(),
+            'time_circle': self.time_circle(),
+            'time_indicator': self.time_indicator(),
+            'time_tick_0': self.time_ticks(0),
+            'time_tick_6': self.time_ticks(6),
+            'time_tick_12': self.time_ticks(12),
+            'time_tick_18': self.time_ticks(18),
 
-            'sr_bar': sr_bar,
-            'ss_bar': ss_bar,
+            'sr_bar': self.sun_bars(self.angle_sunrise),
+            'ss_bar': self.sun_bars(self.angle_sunset),
 
-            'sr_time': sr_time,
-            'ss_time': ss_time,
+            'sr_time': self.sun_times('rise'),
+            'ss_time': self.sun_times('set'),
             
-            'sr_hiding_box': sr_hiding_box,
-            'ss_hiding_box': ss_hiding_box,
+            'sr_hiding_box': self.sun_hiding_boxes('rise'),
+            'ss_hiding_box': self.sun_hiding_boxes('set'),
 
-            'sun': sun,
+            'sun': self.sun(),
         }
 
-        print(json.dumps(var_dict, indent=4))
+        with open(r'C:\Users\Huy\Documents\GitHub\Tasker\Zooper\zooper_variables.txt', 'w') as f:
+            f.write(json.dumps(var_dict, indent=4))
 
     #region time
     def time(self):
-        return y_offset(-self.y_offset)
+        text = y_offset(-self.y_offset)
+        text += text_size(self.radius_ring / 2)
+        return text
 
     def time_circle(self):
         text = ''
@@ -74,13 +68,12 @@ class clock_widget:
         text += rect_width(self.circumference)
         text += rect_height(4)
 
-        text += color('20ffffff')
+        text += color('30ffffff')
 
         return text
 
     def time_indicator(self):
-        text = ''
-        text += y_offset(-self.y_offset)
+        text = y_offset(-self.y_offset)
         text += circle_radius(self.radius_ring)
 
         text += rect_width(8)
@@ -89,6 +82,32 @@ class clock_widget:
         angle = condition(f'(({hour} * 60 + {minutes}) / {mins_in_day}) * 360 + 180')
         text += circle_angle(angle)
         text += rotation(angle)
+
+        return text
+    
+    def time_ticks(self, loc):
+        text = y_offset(-self.y_offset)
+        text += circle_radius(self.radius_ring)
+        text += color('80ffffff')
+
+        tick_height = 6
+        tick_width = 2
+
+        text += rect_height(tick_height)
+        text += rect_width(tick_width)
+
+        if loc == 0:
+            text += circle_angle(180)
+            text += rotation(0)
+        elif loc == 6:
+            text += circle_angle(270)
+            text += rotation(90)
+        elif loc == 12:
+            text += circle_angle(0)
+            text += rotation(0)
+        elif loc == 18:
+            text += circle_angle(90)
+            text += rotation(90)
 
         return text
     #endregion time
@@ -108,6 +127,7 @@ class clock_widget:
 
     def sun_times(self, rise_set):
         text = self.text_sun
+        text += text_size(12)
 
         if rise_set == 'rise':
             angle = self.angle_sunrise
@@ -135,19 +155,21 @@ class clock_widget:
             angle = self.angle_sunset
             sign = '+'
 
-        text += circle_angle(condition(f'{angle} {sign} 4'))
+        text += circle_angle(condition(f'{angle} {sign} 6'))
         text += rotation(angle)
 
         return text
     
     def sun(self):
-        text = self.text_sun
+        text = y_offset(-self.y_offset)
+        text += circle_radius(self.radius_outer - 10)
 
         angle = condition(f'{self.angle_sunrise} + (({self.angle_sunset} - {self.angle_sunrise}) * ({min_since_sr} / ({ss_in_min} - {sr_in_min})))')
         text += circle_angle(angle)
 
-        c = if_else(f'[{hour} >= {sr_h} && {hour} < {ss_h}] || [{hour} = {ss_h} && {minutes}<={ss_min}]', 
-                    'ffffffff', '00000000')
+        sun_up = f'[{hour} >= {sr_h} && {hour} < {ss_h}] || [{hour} = {ss_h} && {minutes}<={ss_min}]'
+
+        c = if_else(sun_up, 'ffffffff', '00000000')
         text += color(c)
 
         # print(text)
@@ -169,32 +191,35 @@ class clock_widget:
 #region functions
 #region standard
 def x_offset(o):
-    return f'[ox]{o}[/ox]\\n'
+    return f'[ox]{o}[/ox]\n'
 
 def y_offset(o):
-    return f'[oy]{o}[/oy]\\n'
+    return f'[oy]{o}[/oy]\n'
 
 def rotation(r):
-    return f'[r]{r}[/r]\\n'
+    return f'[r]{r}[/r]\n'
 
 def color(c):
-    return f'[c]{c}[/c]\\n'
+    return f'[c]{c}[/c]\n'
+
+def text_size(s):
+    return f'[s]{s}[/s]\n'
 #endregion
 
 #region circle
 def circle_radius(r):
-    return f'[ar]{r}[/ar]\\n'
+    return f'[ar]{r}[/ar]\n'
 
 def circle_angle(a):
-    return f'[as]{a}[/as]\\n'
+    return f'[as]{a}[/as]\n'
 #endregion circle
 
 #region rectangle
 def rect_width(w):
-    return f'[rw]{w}[/rw]\\n'
+    return f'[rw]{w}[/rw]\n'
 
 def rect_height(h):
-    return f'[rh]{h}[/rh]\\n'
+    return f'[rh]{h}[/rh]\n'
 #endregion rectangle
 
 #region logic
